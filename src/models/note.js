0658e89feedf4f8309c448880e801664f709e47d
Anastasia <aleksandrovich2109@gmail.com>
2025-11-15 13:00:11 +0200

    hw3

--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new mongoose.Schema(
   {
@@ -6,24 +7,15 @@ const noteSchema = new mongoose.Schema(
     content: { type: String, trim: true, default: '' },
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: TAGS,
       default: 'Todo',
       trim: true,
     },
   },
   { timestamps: true }
 );
+
+noteSchema.index({ title: 'text', content: 'text' });
 
 const Note = mongoose.model('Note', noteSchema);
 export default Note;
